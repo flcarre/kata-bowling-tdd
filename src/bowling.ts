e5fc283e3f95c163af1349ba531d45690b835f53
thominimalist <thomas.ruchon@icloud.com>
2022-04-25 15:26:13 +0200

    add test

--- src/bowling.ts
+++ src/bowling.ts
@@ -2,8 +2,15 @@ export class Game {
     pinsKnocked: Array<number> = [];
 
     score(): number {
-        if(this.pinsKnocked.length === 12) return 300
-        return this.pinsKnocked.reduce((previous, current) => previous + current, 0);
+        if(this.pinsKnocked.length === 12) return 300;
+
+
+        return this.pinsKnocked.reduce((previous, current, index) => {
+          if(current === 10) {
+            return previous + current + this.pinsKnocked[index+1] + this.pinsKnocked[index+2]
+          }
+          return previous + current
+        }, 0);
     }
 
     roll(pinsKnocked: number) {
